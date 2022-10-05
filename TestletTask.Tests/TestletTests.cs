7f0cf3095cdbcc9d4fbca7a3ce734d1346877460
Yury Baranikhin <ybaranikhin@ozon.ru>
2022-10-05 16:02:35 +0400

    Add some tests

---
+++ TestletTask.Tests/TestletTests.cs
@@ -0,0 +1,89 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Xunit;
+
+namespace TestletTask.Tests;
+
+public class TestletTests
+{
+    private Fixture _fixture = new();
+    private const int TestletItemsCount = 10;
+    private const int TestletPretestItemsCount = 4;
+    private const int TestletOperationalItemsCount = 6;
+
+    [Fact]
+    public void Randomize_should_not_change_testlet_items()
+    {
+        // arrange
+        var originalItems = CreateInitialTestletItemsCollection();
+        var testlet = CreateTestlet(originalItems);
+
+        // act
+        var result = testlet.Randomize();
+
+        // assert
+        result.Should().BeEquivalentTo(originalItems);
+    }
+
+    [Fact]
+    // The requirement is that the _order_ of these items should be randomized such that -
+    // The first 2 items are always pretest items selected randomly from the 4 pretest items.
+    // The next 8 items are mix of pretest and operational items ordered randomly from the remaining 8
+    //items.
+    public void Randomize_should_return_items_according_to_required_order()
+    {
+        // arrange
+        var testlet = CreateTestlet();
+
+        // act
+        var result = testlet.Randomize();
+
+        // assert
+        result.Should().HaveCount(TestletItemsCount);
+        result.Take(2).Should().OnlyContain(x => x.ItemType == ItemTypeEnum.Pretest);
+        result.Skip(2).Should()
+            .Match(x => x.Count(y => y.ItemType == ItemTypeEnum.Pretest) == 2)
+            .And
+            .Match(x => x.Count(y => y.ItemType == ItemTypeEnum.Operational) == 6);
+    }
+
+    [Fact]
+    // might be fragile
+    // ToDo: init randomizer with different seeds
+    public void Randomize_should_return_different_results()
+    {
+        // arrange
+        var testlet = CreateTestlet();
+
+        // act
+        var result1 = testlet.Randomize();
+        var result2 = testlet.Randomize();
+
+        // assert
+        result1.Should().NotContainInOrder(result2);
+    }
+
+    private Testlet CreateTestlet(IReadOnlyList<Item>? items = null)
+    {
+        return new Testlet(_fixture.Create<string>(),
+            items?.ToList() ?? CreateInitialTestletItemsCollection().ToList());
+    }
+
+    private IReadOnlyList<Item> CreateInitialTestletItemsCollection()
+    {
+        var result = new List<Item>(TestletPretestItemsCount + TestletOperationalItemsCount);
+
+        var pretestItems = _fixture.Build<Item>()
+            .With(x => x.ItemType, ItemTypeEnum.Pretest)
+            .CreateMany(TestletPretestItemsCount);
+
+        var operationalItems = _fixture.Build<Item>()
+            .With(x => x.ItemType, ItemTypeEnum.Operational)
+            .CreateMany(TestletOperationalItemsCount);
+
+        result.AddRange(pretestItems);
+        result.AddRange(operationalItems);
+
+        return result;
+    }
+}
