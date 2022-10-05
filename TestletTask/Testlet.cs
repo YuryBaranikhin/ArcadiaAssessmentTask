f53f1b97898198bc6b1de82bcde79fcbc12b1412
Yury Baranikhin <ybaranikhin@ozon.ru>
2022-10-05 16:39:07 +0400

    Task implementation

--- TestletTask/Testlet.cs
+++ TestletTask/Testlet.cs
@@ -4,13 +4,40 @@ public class Testlet
 {
     public string TestletId;
     private List<Item> Items;
+
+    private readonly Random Random;
+
     public Testlet(string testletId, List<Item> items)
     {
         TestletId = testletId;
         Items = items;
+
+        Random = new Random();
     }
+
     public List<Item> Randomize()
     {
-        throw new NotImplementedException();
+        var result = new List<Item>(Items.Count);
+
+        result.AddRange(Shuffle(Items.Where(x => x.ItemType == ItemTypeEnum.Pretest).ToList()));
+        result.AddRange(Shuffle(Items.Except(result).ToList()));
+
+        return result;
+    }
+
+    /// <summary>
+    /// <see href="https://en.wikipedia.org/wiki/Fisher–Yates_shuffle">Fisher–Yates shuffle</see>
+    /// </summary>
+    private IReadOnlyList<Item> Shuffle(IReadOnlyList<Item> itemsToShuffle)
+    {
+        var result = new List<Item>(itemsToShuffle);
+
+        for (var n = result.Count - 1; n > 0; --n)
+        {
+            var k = Random.Next(n + 1);
+            (result[n], result[k]) = (result[k], result[n]);
+        }
+
+        return result;
     }
 }
